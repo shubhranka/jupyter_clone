fc53192aeae0ed9adcf8ea5492436b0cc88dfc04
shubhrankavarma@gmail.com <shubhrankavarma@gmail.com>
2022-08-29 19:02:18 +0530

    added esbuild-wasm

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { useEffect, useRef, useState } from "react";
+import esbuildWasm from "esbuild-wasm";
 const App:React.FC = () => {
+
+    const [initialized, setInitialized] = useState(false);
+    const [input, setInput] = useState<string>('');
+    const [code, setCode] = useState<string>('');
+
+    useEffect(()=>{
+        (async ()=>{
+            await esbuildWasm.initialize({
+                wasmURL: '/esbuild.wasm'
+            });
+            setInitialized(true);
+        })()
+        console.log("hello");
+    },[])
     return (
         <div>
-        <h1>Hello World</h1>
+         <textarea onChange={(e)=>setInput(e.target.value)} value={input}></textarea>
+         <button onClick={ async()=>{
+            if(!initialized)
+                return
+            const data = await esbuildWasm.transform(input,{
+                loader:"tsx",
+                target:"es2015",
+            })
+            setCode(data.code);
+         }}>Submit</button>
+         <pre>{code}</pre>
         </div>
     );
 }
