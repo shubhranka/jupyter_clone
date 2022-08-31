a546b3c969a15d3ef7ca393652599d8ea1356ed9
shubhrankavarma@gmail.com <shubhrankavarma@gmail.com>
2022-08-31 14:53:40 +0530

    added code js support

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,79 @@
 import { useEffect, useRef, useState } from "react";
 import esbuildWasm from "esbuild-wasm";
+import { unpkgPathPlugin } from "./unpkgPlugin";
 const App:React.FC = () => {
 
     const [initialized, setInitialized] = useState(false);
     const [input, setInput] = useState<string>('');
-    const [code, setCode] = useState<string>('');
+    const iframeRef = useRef<HTMLIFrameElement>(null);
 
+    const onSubmitListener = async()=>{
+        if(!initialized)
+            return
+        
+        if(iframeRef.current)
+        iframeRef.current.srcdoc = 
+        `
+        <html>
+            <body>
+                <div id="root"></div>
+            </body>
+            <script>
+            window.addEventListener('message', (e) => {
+                try{
+                    eval(e.data);
+                }catch(e){
+                    const errorTitle = e.stack.split(':')[0];
+                    console.log(errorTitle);
+                    document.body.innerHTML+= '<pre style="color:red">'+ '<h3>' + errorTitle+' :' + '</h3>' + e.message + '</pre>';
+                    console.log({e});
+                }
+                
+            },false);
+            </script>
+        </html>
+        `;
+        const data = await esbuildWasm.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            define:{
+                'process.env.NODE_ENV': '"development"',
+                global: 'window'
+            },
+            plugins: [unpkgPathPlugin(input)]
+        })
+        // console.log(data.outputFiles[0].text);
+        // setCode(data.outputFiles[0].text);
+
+
+        if(iframeRef.current?.contentWindow){
+            iframeRef.current.contentWindow.postMessage(data.outputFiles[0].text, '*');
+        }
+     }
     useEffect(()=>{
         (async ()=>{
-            await esbuildWasm.initialize({
-                wasmURL: '/esbuild.wasm'
-            });
+            if(!initialized)
+            try{
+                await esbuildWasm.initialize({
+                    wasmURL: "https://unpkg.com/esbuild-wasm@0.15.5/esbuild.wasm"
+                });
+            }catch(e){
+                console.error(e);
+            }
             setInitialized(true);
         })()
-        console.log("hello");
-    },[])
+        // console.log("hello");
+    },[initialized])
     return (
         <div>
-         <textarea onChange={(e)=>setInput(e.target.value)} value={input}></textarea>
-         <button onClick={ async()=>{
-            if(!initialized)
-                return
-            const data = await esbuildWasm.transform(input,{
-                loader:"tsx",
-                target:"es2015",
-            })
-            setCode(data.code);
-         }}>Submit</button>
-         <pre>{code}</pre>
+         <textarea onChange={(e)=>setInput(e.target.value)} value={input} onKeyUp={e=>{
+                if(e.key === 'Enter' && e.ctrlKey){
+                    onSubmitListener();
+                }
+         }}></textarea>
+         <button onClick={ onSubmitListener }>Submit</button>
+         <iframe ref={iframeRef} sandbox="allow-scripts" title="myIframe"></iframe>
         </div>
     );
 }
--- src/unpkgPlugin.ts
+++ src/unpkgPlugin.ts
@@ -1,30 +1,75 @@
 import * as esbuild from 'esbuild-wasm';
+import localforage from 'localforage';
 
-export const unpkgPathPlugin = () => {
+const cacheInstance:any = localforage.createInstance({
+  name: 'unpkgPlugin',
+});
+
+// Make cacheInstance available to global scope
+(window as any).localForageCacheInstance = cacheInstance;
+export const unpkgPathPlugin = (input:string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args);
-        return { path: args.path, namespace: 'a' };
+        // console.log('onResole', args);
+        
+        if(args.path === "index.js") {
+          return { path: args.path, namespace: 'a' };
+        }
+        else{
+          if(!args.path.startsWith(".")){
+            const url = new URL(args.path, "https://unpkg.com/");
+            return { path:url.href , namespace: 'a'};
+          }else {
+            const url = new URL(args.path,args.resolveDir.substring(1)+"/")
+            return { path: url.href, namespace: 'a'};
+          }
+        }
+
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
+        // console.log('onLoad', args);
 
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              import message from './message';
-              console.log(message);
-            `,
+            contents: `${input}`,
+            resolveDir: '.',
           };
         } else {
-          return {
-            loader: 'jsx',
-            contents: 'export default "hi there!"',
+          const key = args.path;
+          const myResult = cacheInstance.getItem(key);
+          if (myResult) {
+            return myResult;
+          }
+          const data:any = await fetch(args.path);
+          const ind = data.url.lastIndexOf("/");
+          const redirectedURL = data.url.substring(0, ind)
+
+          const fileType = args.path.endsWith('css') ? 'css' : 'jsx';
+          // const redirectedURL = new URL("./",data.url)
+          let dataContent = await data.text();
+          if(fileType === 'css')
+          dataContent = dataContent
+          .replace(/\n/g, ' ')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+
+          const contents = fileType === 'css' ? `
+            const style = document.createElement('style');
+            style.innerHTML = "${dataContent}";
+            document.head.appendChild(style);
+          ` : dataContent;
+
+          const result: esbuild.OnLoadResult = {
+            loader:"jsx",
+            contents,
+            resolveDir: redirectedURL,
           };
+          await cacheInstance.setItem(key, result);
+          return result;
         }
       });
     },
